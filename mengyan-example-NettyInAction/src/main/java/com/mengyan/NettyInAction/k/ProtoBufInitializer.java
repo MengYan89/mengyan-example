ac111ce56f56737851691d5d105481694fb249ed
mengyan <my327827856@163.com>
2021-03-31 19:04:57 +0800

    Netty In Action 11-14 使用protobuf

---
+++ mengyan-example-NettyInAction/src/main/java/com/mengyan/NettyInAction/k/ProtoBufInitializer.java
@@ -0,0 +1,38 @@
+package com.mengyan.NettyInAction.k;
+
+import com.google.protobuf.MessageLite;
+import io.netty.channel.*;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+
+/**
+ * 11-14 使用protobuf
+ */
+public class ProtoBufInitializer extends ChannelInitializer<Channel> {
+    private final MessageLite lite;
+
+    public ProtoBufInitializer(MessageLite lite) {
+        this.lite = lite;
+    }
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+        // 添加ProtobufVarint32FrameDecoder以分隔帧
+        pipeline.addLast(new ProtobufVarint32FrameDecoder());
+        // 添加ProtobufEncoder以处理消息的编码
+        pipeline.addLast(new ProtobufEncoder());
+        // 添加ProtobufDecoder以解码消息
+        pipeline.addLast(new ProtobufDecoder(lite));
+        // 添加ObjectHandler处理解码消息
+        pipeline.addLast(new ObjectHandler());
+    }
+
+    public static final class ObjectHandler extends SimpleChannelInboundHandler<Object> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+            // Do something with the object
+        }
+    }
+}
