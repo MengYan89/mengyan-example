66bac32faba0e27e9dd026fd35dafbc0dbefe6ee
mengyan <my327827856@163.com>
2021-04-02 02:06:18 +0800

    Netty In Action 12-3 初始化ChannelPipeline

---
+++ mengyan-example-NettyInAction/src/main/java/com/mengyan/NettyInAction/l/ChatServerInitializer.java
@@ -0,0 +1,33 @@
+package com.mengyan.NettyInAction.l;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * 12-3 初始化ChannelPipeline
+ */
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+    private final ChannelGroup group;
+
+    public ChatServerInitializer(ChannelGroup group) {
+        this.group = group;
+    }
+
+    // 将所有需要的ChannelHandler添加到ChannelPipeline中
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        pipeline.addLast(new HttpRequestHandler("/ws"));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler(group));
+    }
+}
